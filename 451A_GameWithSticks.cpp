2752cf263983b492a454a0b3ddcdbd5539e46be1
Arman-2107046 <armanr.rafi@gmail.com>
2025-03-05 12:23:43 +0600

    451A done

---
+++ 451A_GameWithSticks.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main()
+{
+ll n,m;
+cin>>n>>m;
+
+ll mini=min(n,m);
+if(mini%2==0)
+{
+    cout<<"Malvika";
+}
+else
+{
+    cout<<"Akshat";
+}
+
+}
